075b1d80162fe63cfc66f82ce6367b1ff8421a48
Heidi <heidi37@users.noreply.github.com>
2019-11-07 07:25:17 -0500

    active links working intial page not working will only work by clicking pages in reverse order

--- js/script.js
+++ js/script.js
@@ -6,48 +6,25 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 
 const listItems = document.getElementsByClassName('student-item');
-
 const pageItems = 10;
 
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
 ***/
 
 function showPage(list, page) {
-   const startIndex = (page * pageItems) - pageItems;
-   const endIndex = page * pageItems;
-   for (i = 0; i < list.length; i++) {
+   let startIndex = (page * pageItems) - pageItems;
+   let endIndex = page * pageItems;
+   for (let i = 0; i < list.length; i++) {
       if(i >= startIndex && i < endIndex){
          list[i].style.display = "block";
       }
    }
 }
-
-showPage(listItems, 1);
 
 
 /*** 
@@ -62,17 +39,38 @@ const nodeUl = document.createElement("UL");
 nodeDiv.appendChild(nodeUl);
 nodeDiv.className = "pagination";
 
-   for(i = 1; i <= listItems.length / 10 ; i++){
+   for(let j = 1; j <= listItems.length / 10 ; j++){
       const nodeLi = document.createElement("LI");
-      nodeUl.appendChild(nodeLi)
-      nodeLi.innerHTML = '<a href="#">' + i + '</a>';
+      nodeUl.appendChild(nodeLi);
+      nodeLi.innerHTML = '<a href="#">' + j + '</a>';
    }
 
+document.querySelector('.page').appendChild(nodeDiv);
+
+// on intital page load of page 1, set the 1st a tag to active
 nodeUl.firstChild.firstChild.className = "active";
 
-document.querySelector('.page').appendChild(nodeDiv);
+let aLinks = document.getElementsByTagName('a');
+
+   //add event listener to the a tags
+   for(let k = 0; k < aLinks.length; k++){
+      
+      aLinks[k].addEventListener('click', (event) => {
+      let targetedEvent = event.target;
+      let pageNumber = targetedEvent.innerHTML;
+      showPage(listItems, pageNumber);
+      console.log(pageNumber);
+      console.log(targetedEvent);
+
+         for(let l = 0; l < aLinks.length; l++){
+            aLinks[l].classList.remove("active");
+            }
+      targetedEvent.classList.add("active");
+      });
+   }
 }
 
+
+showPage(listItems, 5);
 appendPageLinks(listItems);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
